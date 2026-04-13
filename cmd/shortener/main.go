139dcbd7925400adab1959d64a2ec661cabcb9a9
Constantine <tmail99@gmail.com>
2026-04-13 14:18:49 +0300

    first step to iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,13 +6,27 @@ import (
 	"time"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+// hand - Snippet for http handler declaration
+func ShortenText(w http.ResponseWriter, r *http.Request) {
+	// Здесь я должен получить текст из тела запроса (строку URL как text/plain)
+	// и вернуть ответ с кодом 201 и сокращённым URL как text/plain
+	// Видимо для начала разрешается фиксированная строка, к примеру "/EwHXdJfB"
+	//
+	// Все негативные кейсы- возвращаем 400
+}
+
+func Expand(w http.ResponseWriter, r *http.Request) {
+	// Эндпоинт с методом GET и путём /{id}, где id — идентификатор сокращённого URL (например, /EwHXdJfB).
+	// В случае успешной обработки запроса сервер возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
+	//
+	// Все негативные кейсы- возвращаем 400
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", greet)
+	mux.HandleFunc("POST /", ShortenText)
+	mux.HandleFunc("GET /EwHXdJfB", Expand)
 
 	// Вторым параметром ListenAndServe получает:
 	// mux (маршрутизатор= роутер= multiplexer) или
