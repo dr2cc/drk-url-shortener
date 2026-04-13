9e3a42f272948a95dc3aef2b6774e11d16be2e21
Constantine <tmail99@gmail.com>
2026-04-13 13:29:52 +0300

    helloweb snippet +

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", greet)
+
+	// Вторым параметром ListenAndServe получает:
+	// mux (маршрутизатор= роутер= multiplexer) или
+	// nil (используется маршрутизатор http.DefaultServeMux).
+	http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module drk-url-shortener
+
+go 1.26.1
