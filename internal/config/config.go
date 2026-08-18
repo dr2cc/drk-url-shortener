5a906346663333752acaab031f2e15bbe2fb1972
Constantine <tmail99@gmail.com>
2026-08-18 17:08:47 +0300

    minor

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,19 +17,24 @@ type Config struct {
 
 func New() (Config, error) {
 	cfg := Config{}
+	// 1. Присваиваем полям Config флаги
 	// Разбираем флаги в конфигурацию
 	flag.StringVar(&cfg.ServAddres, "a", ":8080", "HTTP server startup address")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "base URL")
 	flag.StringVar(&cfg.CacheDumpPath, "f", "./short-url-db.json", "path to dump with addresses")
 	flag.Parse()
 
-	// // Удобная система, но не соответствует заданию yp.
-	// // Загружаем переменные окружения из файла .env в корне
+	// // 2. Загужаем в окружение переменные из файла .env в корне.
+	// // Не соответствует заданию yp, но для других проектов достаточно снять комментарий.
 	// if err := godotenv.Load(); err != nil {
 	// 	log.Fatalf("Error loading env variables: %s", err.Error())
 	// }
 
-	// Разбираем переменные окружения в конфигурацию (caarlos0/env)
+	// parse читает переменные окружения и заполняет структуру cfg . Подробнее:
+	// - Библиотека берёт структуру cfg и с помощью механизма рефлексии (способность программы изучать саму себя) смотрит на её поля.
+	// - Она ищет поля, у которых есть тег env:"ИМЯ_ПЕРЕМЕННОЙ".
+	// - Если в самой операционной системе (в окружении) есть переменная с таким же именем,
+	// библиотека caarlos0/env берёт её значение и записывает в это конкретное поле.
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("Config parsing error: %+v\n", err)
 		return Config{}, err
